417a8238a2b38382174c0340586dacddaefd9ecd
Urs Wilke <urs.wilke@gmail.com>
2023-06-12 19:17:29 +0200

    don't read column names from marco sheet

--- R/options.R
+++ R/options.R
@@ -12,9 +12,13 @@ setOptions <- function(mapping_file) {
   df_macro_raw <- readxl::read_excel(
     mapping_file,
     sheet = "Macro",
-    col_types = "text"
-  )
-  colvar <- df_macro_raw[4, 4 + v_scenario][[1]] |> stringr::str_split_1("[ ,;]+")
+    col_types = "text",
+    col_names = FALSE
+  ) |>
+    suppressMessages()
+
+  names(df_macro_raw) <- paste0("X", seq_len(ncol(df_macro_raw)))
+  colvar <- df_macro_raw[5, 4 + v_scenario][[1]] |> stringr::str_split_1("[ ,;]+")
 
   return(tibble::lst(
     v_scenario,
