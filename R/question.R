1fe49145a8949dfd1c37e192eb1642bf7e7f4ea9
urswilke <urs.wilke@gmail.com>
2023-06-13 23:59:21 +0200

    - allow for multiple `SelVar`s - allow for relabeling of SelVal intervals - add SelVal subtitles to Title column

--- R/question.R
+++ R/question.R
@@ -28,7 +28,7 @@ process_qsheet <- function(mapping) {
         purrr::map(\(x) x |> append(mapping$options$l_macro_scenario$Filter)) |>
         purrr::map(\(x) x[!is.na(x)]),
       SelVar = split_cell(SelVar),
-      SelVal = split_cell(SelVal) |> purrr::map(as.numeric),
+      SelVal = split_cell(SelVal),
       RvEmp = stringr::str_trim(RvEmp) == "EXCLUDE"
     ) |>
     unnest_qsheet_rows(mapping)
@@ -38,6 +38,8 @@ unnest_qsheet_rows <- function(df_qsheet, mapping) {
   df_qsheet |>
     row_split() |>
     purrr::map_dfr(\(row) unnest_selvar(row, mapping)) |>
+    row_split() |>
+    purrr::map_dfr(\(row) unnest_selval(row, mapping)) |>
     row_split() |>
     purrr::map_dfr(\(row) unnest_mw_rows(row, mapping))
 }
@@ -65,16 +67,59 @@ unnest_mw_rows <- function(df_row, mapping) {
   res
 }
 unnest_selvar <- function(df_row, mapping) {
-  if (is.na(df_row$SelVar[[1]])) {
+  selvars <- df_row$SelVar[[1]]
+  if (is.na(selvars[1])) {
+    return(df_row)
+  }
+  rowvars <- df_row$RowVar[[1]]
+  n_selvar <- length(selvars)
+  res <- df_row[rep(1, n_selvar),]
+  res$SelVar <- selvars |> as.list()
+  for (i_row in seq_len(n_selvar)) {
+    res[i_row,]$RowVar <- rowvars[seq(
+      i_row,
+      length(rowvars),
+      n_selvar
+    )] |>
+      list()
+  }
+
+  res
+}
+unnest_selval <- function(df_row, mapping) {
+  if (is.na(df_row$SelVar[[1]][1])) {
     return(df_row)
   }
   n_selval <- length(df_row$SelVal[[1]])
   res <- df_row[rep(1, n_selval),]
-  # TODO: generalise for selval expressions like "1-3:new_label"
+  selvar_vallabs <- attr(mapping$dat_mod[[df_row$SelVar[[1]]]], "labels")
+  selval_relabels <- df_row$SelVal[[1]] |> stringr::str_extract("(?<=:)[^ ]+") |> stringr::str_replace_all("_", " ")
+  selvals <- df_row$SelVal[[1]] |>
+    # stringr::str_extract("^\\d+") |>
+    as.numeric() |>
+    suppressWarnings()
+
+  subtitles <- dplyr::coalesce(
+    selval_relabels,
+    selvar_vallabs[match(selvals, selvar_vallabs)] |> names()
+  )
+  res$Title |> purrr::map2(
+    subtitles,
+    \(title, subtitle) title |> append(subtitle))
+
+  selval_intervals <- df_row$SelVal[[1]] |> stringr::str_remove(paste0(":?", subtitles))
   res$Filter <- purrr::map2(
     res$Filter,
-    df_row$SelVal[[1]],
-    \(filt, selval) append(filt, paste0(df_row$SelVar, " == ", selval))
+    selval_intervals,
+    \(filt, selval) append(filt, write_selval_filter_string(df_row$SelVar, selval))
   )
   res
+}
+
+write_selval_filter_string <- function(selvar, selval) {
+  if (!is.na(as.numeric(selval) |> suppressWarnings())) {
+    return(paste0(selvar, " == ", selval))
+  }
+  selval_interval <- stringr::str_split_1(selval, "-")
+  paste0(selvar, " >= ", selval_interval[1], " & ", selvar, " <= ", selval_interval[2])
 }
