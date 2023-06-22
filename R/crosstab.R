238cbafdfd063e300ae6d763fe74c6e95819c7ef
Urs Wilke <urs.wilke@gmail.com>
2023-06-22 12:19:01 +0200

    move to other file

--- R/crosstab.R
+++ R/crosstab.R
@@ -1,3 +1,80 @@
+crosstab <- function(df_row, long_data, mapping) {
+  UseMethod("crosstab")
+}
+crosstab.tab_type_cat <- function(df_row, long_data, mapping) {
+  weight <- dplyr::coalesce(mapping$options$l_macro_scenario$Weight, df_row$Weight)
+  stat_fun <- df_row$ZsfgMW
+  total_row_data <- long_data |>
+    dplyr::group_by(dplyr::across(-matches("weight|rowva[rl]"))) |>
+    new_sum_stat(weight, NA) |>
+    apply_sum_stat()
+  total_row_data$rowvar <- paste0(long_data$rowvar[1], "_TC")
+  total_row_data$rowval <- 1
+  if (!is.na(df_row$CatRec)) {
+    long_data_catrec <- gen_catrec_long_data(df_row, long_data, mapping)
+    long_data <- dplyr::bind_rows(long_data, long_data_catrec)
+  }
+  all_counts <- long_data |>
+    dplyr::group_by(dplyr::across(-matches("weight"))) |>
+    new_sum_stat(weight, stat_fun) |>
+    apply_sum_stat()
+  rbind(
+    total_row_data,
+    all_counts
+  )
+}
+gen_catrec_long_data <- function(df_row, long_data, mapping) {
+  cat_rec_string <- df_row$CatRec
+  cat_lab_string <- df_row$CatLab
+  cat_rec_interval_splits <- split_cat_rec_string(cat_rec_string)
+  cat_lab_splits <- split_cat_lab_string(cat_lab_string)
+  cat_rec_quos <- lapply(cat_rec_interval_splits$interval_strings, gen_cat_rec_fun)
+  cat_rec_exprs <- stringr::str_extract_all(cat_rec_string, "(?<=\\().*?(?=\\))")[[1]]
+  cat_rec_vals <- stringr::str_extract(cat_rec_exprs, "(?<=\\=) *\\d+$") |> as.numeric()
+
+  invalid_vals <- dplyr::coalesce(df_row$Unguelt[[1]], mapping$options$l_macro_scenario$Unguelt)
+
+  long_data_catrec <- long_data[!long_data$rowval %in% invalid_vals,]
+  # TODO: find cleander solution where `vec` doesn't need to be calculated in advance!
+  vec <- long_data_catrec$rowval
+  l_cat_rec <- purrr::map2(
+    cat_rec_quos,
+    cat_rec_vals,
+    \(f, x) rlang::quo(!!f(vec) ~ !!x)
+  )
+  long_data_catrec$rowvar <- paste0(long_data_catrec$rowvar, "__summary")
+  long_data_catrec$rowval <- dplyr::case_when(!!!l_cat_rec)
+  # TODO: also tabulate non-recoded (not covered by CatRec) valid `RowVal`s:
+  non_recoded_idx <- is.na(long_data_catrec$rowval)
+  if (any(non_recoded_idx)) {
+    warning(
+      "\nIn table in row ", df_row$row, ":\n",
+      "These valid values are not recoded by CatRec: ",
+      vec[non_recoded_idx] |> unique(),
+      "\nTabulation not implemented yet!!!"
+    )
+    long_data_catrec <- long_data_catrec[!non_recoded_idx,]
+  }
+  long_data_catrec
+}
+
+crosstab.tab_type_mw <- function(df_row, long_data, mapping) {
+  weight <- dplyr::coalesce(mapping$options$l_macro_scenario$Weight, df_row$Weight)
+  stat_fun <- dplyr::coalesce(df_row$ZsfgMW, "mean")
+  invalid_vals <- df_row$Unguelt[[1]]
+  if (is.na(invalid_vals[1])) {
+    invalid_vals <- mapping$options$l_macro_scenario$Unguelt
+  }
+
+  long_data[!long_data$rowval %in% invalid_vals,] |>
+    dplyr::group_by(dplyr::across(-matches("weight|rowval"))) |>
+    new_sum_stat(weight, stat_fun) |>
+    apply_sum_stat()
+}
+crosstab.tab_type_mcg <- crosstab.tab_type_cat
+crosstab.tab_type_mdg <- crosstab.tab_type_cat
+
+
 # hack to do double dispatch on is.na(weight) & stat_fun:
 new_sum_stat <- function(df_long, weight, stat_fun) {
   subclass_str <- dplyr::case_when(
