7b5b236718a8c7539c005816d12ad366b6f1469b
Urs Wilke <urs.wilke@gmail.com>
2023-06-24 00:20:57 +0200

    adapt test to refactored code

--- tests/testthat/test-tabula.R
+++ tests/testthat/test-tabula.R
@@ -2,10 +2,9 @@ spss_file <- "K:/Projects/UW git/crosstabser/tests/testthat/spss/fake_survey.sav
 spss_file <- "spss/fake_survey.sav"
 mapping_file <- "K:/Projects/UW git/crosstabser/tests/testthat/excel/mapping.xlsx"
 mapping_file <- "excel/mapping.xlsx"
-tt <- Tabula$new(spss_file, mapping_file)
-tt$tabulate_qsheet_rows()
+tt <- Tabula$new(spss_file, mapping_file)$add_tab_data()
 
-df_crosstab_cat <- tt$qsheet$tables$counts[[1]] |>
+df_crosstab_cat <- tt$tabs[[1]]$d$counts |>
   dplyr::arrange(colvar, colval) |>
   tidyr::pivot_wider(
     names_from = c(colvar, colval),
