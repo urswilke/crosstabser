c807d9ff5df01c6149b3da51ad2753993e42bd61
Urs Wilke <urs.wilke@gmail.com>
2023-06-26 19:07:59 +0200

    fix & refactor: write all tables associated to one question sheet row in one xml file

--- R/write_xml.R
+++ R/write_xml.R
@@ -1,23 +1,26 @@
 write_xml_tables <- function(mapping) {
-  l_5_tables <- lapply(
-    seq_len(length(mapping$tabs)),
-    \(i_tab) gen_5_tables(i_tab, mapping)
+  qsheet_row_idx <- split(
+    mapping$qsheet$tab_table$TabNo,
+    mapping$qsheet$tab_table$row
   )
-  file_names <- paste0("dev/xml/tab", stringr::str_pad(mapping$qsheet$tables$row, 4, pad = "0"), ".xml")
+  l_row_tabs <- qsheet_row_idx |>
+    lapply(\(row) lapply(row, \(i_tab) mapping$tabs[[i_tab]] |> gen_5_tables()))
+
+  file_names <- paste0("dev/xml/tab", stringr::str_pad(names(qsheet_row_idx), 4, pad = "0"), ".xml")
   purrr::map2(
-    l_5_tables,
+    l_row_tabs,
     file_names,
-    \(el_5_tabs, filename) write_xml_table(el_5_tabs, filename)
+    \(l_row, filename) write_xml_file(l_row, filename)
   )
 }
 
-gen_5_tables <- function(i_tab, mapping) {
+gen_5_tables <- function(tab) {
   tabs_ex <- list(
-    Row = mapping$tabs[[i_tab]]$d$row_table,
-    Tab = mapping$tabs[[i_tab]]$d$tab_table,
-    Head = mapping$tabs[[i_tab]]$d$head_table,
-    Col = mapping$tabs[[i_tab]]$d$col_table,
-    Val = mapping$tabs[[i_tab]]$d$val_table
+    Row = tab$d$row_table,
+    Tab = tab$d$tab_table,
+    Head = tab$d$head_table,
+    Col = tab$d$col_table,
+    Val = tab$d$val_table
   )
 
 }
@@ -42,4 +45,31 @@ write_xml_table <- function(el_5_tabs, filename) {
     }
   }
   xml2::write_xml(doc, filename)
+}
+
+write_xml_file <- function(l_row, filename) {
+  names(l_row) <- rep("Table", length(l_row))
+  l_xml <- table_as_xml_list(l_row)
+
+  list(Root = l_xml) |> xml2::as_xml_document() |> xml2::write_xml(filename)
+}
+table_to_line_lists <- function(df) {
+  l <- transpose(df)
+  names(l) <- rep("Line", length(l))
+  l |> lapply(row_to_list_of_lists)
+}
+row_to_list_of_lists <- function(row) {
+  na_idx <- is.na(row)
+  row[na_idx] <- lapply(row[na_idx], \(x) list())
+  row[!na_idx] <- lapply(row[!na_idx], \(x) list(x))
+  row
+}
+
+table_el_as_xml_list <- function(rows) {
+  rows |> lapply(table_to_line_lists)
+}
+
+table_as_xml_list <- function(l_row) {
+  l_row |>
+    lapply(table_el_as_xml_list)
 }
