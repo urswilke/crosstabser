273530fde28bc8840455ed8958dc3cab3a9753f2
Urs Wilke <urs.wilke@gmail.com>
2023-08-16 20:52:16 +0200

    also test (reduced) new mapping

--- tests/testthat/test-tabula.R
+++ tests/testthat/test-tabula.R
@@ -24,3 +24,18 @@ wide_tab <- tt$qrows$qtabs[[1]]$qtabs[[1]]$wide_tab()$d$wide_tab
 test_that("wide_tab is repoduced for cat", {
   testthat::expect_snapshot(wide_tab)
 })
+
+spss_file <- "K:/Projects/UW git/crosstabser/tests/testthat/spss/30_Datenanpassungen_mapping_neu_reduced.sav"
+spss_file <- "spss/30_Datenanpassungen_mapping_neu_reduced.sav"
+mapping_file <- "K:/Projects/UW git/crosstabser/tests/testthat/excel/mapping_neu_reduced.xlsx"
+mapping_file <- "excel/mapping_neu_reduced.xlsx"
+tt <- Tabula$new(spss_file, mapping_file)$calc_qtabs()
+
+wide_tabs <- 1:4 |> lapply(\(i) tt$qrows$qtabs[[i]]$qtabs[[1]]$wide_tab()$d$wide_tab)
+
+# test_that("long_tab repoduced for cat", {
+#   testthat::expect_snapshot(long_tab)
+# })
+test_that("wide_tab is repoduced with new mapping", {
+  testthat::expect_snapshot(wide_tabs)
+})
