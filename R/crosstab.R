951916443ff71aadbd31d8c8eb401f418ebaf357
Urs Wilke <urs.wilke@gmail.com>
2023-08-17 14:53:06 +0200

    don't treat weighting at all for now

--- R/crosstab.R
+++ R/crosstab.R
@@ -55,9 +55,6 @@ calc_stats_rows.qtab_type_cat <- function(qtab) {
     pivot_cols()
 
   row_types <- c("total", "n_valid")
-  if (length(qtab$p$Weight) > 0) {
-    purrr::walk(row_types, \(x) df_cols_long[[x]] <- df_cols_long[[x]] * df_cols_long[[qtab$p$Weight]])
-  }
   df_stats_rows <- stats::aggregate(. ~ colvar + colval, data = df_cols_long, sum) |> dplyr::as_tibble()
   l_row_types <- row_types |>
     purrr::set_names() |>
@@ -99,10 +96,6 @@ calc_stats_rows.qtab_type_mdg <- function(qtab) {
     pivot_cols()
 
   row_types <- c("total", "sum_of_valid", "n_valid", "no_entry")
-  if (length(qtab$p$Weight) > 0) {
-    #TODO: check if that works and is good..:
-    purrr::walk(row_types, \(x) df_cols_long[[x]] <- df_cols_long[[x]] * df_cols_long[[qtab$p$Weight]])
-  }
   df_stats_rows <- stats::aggregate(. ~ colvar + colval, data = df_cols_long, sum) |> dplyr::as_tibble()
   l_row_types <- row_types |>
     purrr::set_names() |>
@@ -152,10 +145,6 @@ calc_stats_rows.qtab_type_mw <- function(qtab) {
     pivot_cols()
 
   row_types <- c("n_valid")
-  if (length(qtab$p$Weight) > 0) {
-    #TODO: check if that works and is good..:
-    purrr::walk(row_types, \(x) df_cols_long[[x]] <- df_cols_long[[x]] * df_cols_long[[qtab$p$Weight]])
-  }
   df_stats_rows <- stats::aggregate(. ~ colvar + colval, data = df_cols_long, sum) |> dplyr::as_tibble()
   l_row_types <- row_types |>
     purrr::set_names() |>
