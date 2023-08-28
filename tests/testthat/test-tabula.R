c835aff581b083c86cc16445f1709b3401fc92f3
Urs Wilke <urs.wilke@gmail.com>
2023-08-28 20:13:07 +0200

    remove reduced spss file

--- tests/testthat/test-tabula.R
+++ tests/testthat/test-tabula.R
@@ -1,5 +1,5 @@
-spss_file <- "K:/Projects/UW git/crosstabser/tests/testthat/spss/30_Datenanpassungen_mapping_neu_reduced.sav"
-spss_file <- "spss/30_Datenanpassungen_mapping_neu_reduced.sav"
+spss_file <- "K:/Projects/UW git/crosstabser/tests/testthat/spss/30_Datenanpassungen_mapping_neu.sav"
+spss_file <- "spss/30_Datenanpassungen_mapping_neu.sav"
 mapping_file <- "K:/Projects/UW git/crosstabser/tests/testthat/excel/mapping_neu_reduced.xlsx"
 mapping_file <- "excel/mapping_neu_reduced.xlsx"
 tt <- Tabula$new(spss_file, mapping_file)$calc_qtabs()
