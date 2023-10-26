52937a7ffcbc7a18f691b3ef035254bdf87c1ab2
Urs Wilke <urs.wilke@gmail.com>
2023-10-27 00:37:15 +0200

    uncomment code to revert 70ba570bea0cfad6da0586892504a2d7152eb10d

--- R/options.R
+++ R/options.R
@@ -6,11 +6,11 @@ setOptions <- function(mapping_file) {
     namedRegion = "V_Scenario",
     colNames = FALSE
   )[[1]]
-  V_ScenName <- openxlsx::read.xlsx(
-    mapping_file,
-    namedRegion = "V_ScenName",
-    colNames = FALSE
-  )[[1]]
+  # V_ScenName <- openxlsx::read.xlsx(
+  #   mapping_file,
+  #   namedRegion = "V_ScenName",
+  #   colNames = FALSE
+  # )[[1]]
   V_Language <- openxlsx::read.xlsx(
     mapping_file,
     namedRegion = "V_Language",
@@ -48,7 +48,7 @@ setOptions <- function(mapping_file) {
 
   return(tibble::lst(
     v_scenario,
-    V_ScenName,
+    # V_ScenName,
     V_Language,
     V_XMLName,
     df_macro_raw,
--- R/qrows.R
+++ R/qrows.R
@@ -9,10 +9,8 @@ Qrow <- R6::R6Class(
                           ...) {
       self$p <- p
       self$m <- mapping
-      self$qtabs <-
-        tibble::tibble(params = process_qrow_params(self$p, self$m))
-      self$qtabs$obj <-
-        new_qtabs(self$qtabs$params, mapping)
+      self$qtabs <- tibble::tibble(params = process_qrow_params(self$p, self$m))
+      self$qtabs$obj <- new_qtabs(self$qtabs$params, mapping)
 
       self$calc_qrow_qtabs()
     },
@@ -25,15 +23,15 @@ Qrow <- R6::R6Class(
       invisible(self)
     },
     xml = function() {
-      scen_name_suffix <-
-        ifelse(
-          self$m$options$V_ScenName == "Standard",
-          "",
-          paste0(self$m$options$V_ScenName, "_")
-        )
+      # TODO: clean up ...!
+      # scen_name_suffix <- ifelse(
+      #   self$m$options$V_ScenName == "Standard",
+      #   "",
+      #   paste0(self$m$options$V_ScenName, "_")
+      # )
       file_name <- paste0(
         self$m$options$V_XMLName,
-        scen_name_suffix,
+        # scen_name_suffix,
         stringr::str_pad(self$p$row, 4, pad = "0"),
         ".xml"
       )
