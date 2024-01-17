e5535680d02a100121bb1855a0b2170488bac73c
Urs Wilke <urs.wilke@gmail.com>
2024-01-17 15:12:17 +0100

    revert 041b9414ee86588b1328edf9da1ae4151ba92af8 (these fields are needed for the filename in the xml export)

--- R/qrows.R
+++ R/qrows.R
@@ -1,11 +1,15 @@
 Qrow <- R6::R6Class(
   "Qrow",
   public = list(
+    p = list(),
+    m = list(),
     qtabs = tibble::tibble(),
     initialize = function(p,
                           mapping,
                           ...) {
-      params <- process_qrow_params(p, mapping)
+      self$p <- p
+      self$m <- mapping
+      params <- process_qrow_params(self$p, self$m)
       obj <- params |>
         lapply(\(x) Qtab$new(x, mapping))
       self$qtabs <- tibble::tibble(params, obj)
