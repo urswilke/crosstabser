aaf903a0d7e4950a3055939379ca86fd9fdb5a1e
Urs Wilke <urs.wilke@gmail.com>
2024-09-06 15:28:12 +0200

    import content of named region cell V_BookNo from mapping

--- R/options.R
+++ R/options.R
@@ -25,6 +25,11 @@ setOptions <- function(mapping_file) {
   ) |>
     suppressMessages()
 
+  V_BookNo <- openxlsx::read.xlsx(
+    mapping_file,
+    namedRegion = "V_BookNo",
+    colNames = FALSE
+  )[[1]]
   names(df_macro_raw) <- paste0("X", seq_len(ncol(df_macro_raw)))
 
   l_macro_scenario <- extract_scenario_options(df_macro_raw, v_scenario)
@@ -44,6 +49,7 @@ setOptions <- function(mapping_file) {
     v_scenario,
     V_Language,
     V_XMLName,
+    V_BookNo,
     df_macro_raw,
     l_macro_scenario,
     l_lexikon,
