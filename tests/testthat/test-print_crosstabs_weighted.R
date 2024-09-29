730485335587230190dfb8c8439c89e8709c1bb9
urswilke <urs.wilke@gmail.com>
2024-09-30 00:21:02 +0200

    use FALSE instead of F

--- tests/testthat/test-print_crosstabs_weighted.R
+++ tests/testthat/test-print_crosstabs_weighted.R
@@ -4,7 +4,7 @@ mapping_file <- "excel/mapping_neu_reduced.xlsx" |> testthat::test_path()
 df <- haven::read_sav(spss_file)
 df$q3a_NA <- df$q3a
 df[df$kregio == 1,]$q3a_NA <- NA_real_
-tabsi <- Tabula$new(df, mapping_file, tabulate = F)
+tabsi <- Tabula$new(df, mapping_file, tabulate = FALSE)
 tabsi$options$l_lexikon["cTabWeighted"] <- "W"
 tabsi$options$l_macro_scenario$Weight <- "gew"
 tabsi$options$l_macro_scenario$Unweight <- TRUE
