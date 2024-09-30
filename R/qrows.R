759baf782378af507a35296589b06f9b0ed28c59
urswilke <urs.wilke@gmail.com>
2024-09-30 19:11:58 +0200

    print errors & warnings if verbose = TRUE

--- R/qrows.R
+++ R/qrows.R
@@ -13,15 +13,18 @@ Qrow <- R6::R6Class(
 
       params <- process_qrow_params(self$p, self$m)
 
+      verbose <- mapping$params$verbose
       obj <- tryCatch(
         error = function(e) {
           self$log$error <- utils::capture.output(e)[-1] |> paste(collapse = "\n")
+          if (verbose) e |> conditionMessage() |> message()
           obj <- list(NULL)
         },
         withCallingHandlers(
           # message = <WE-COULD-ALSO-LOG-MESSAGES..._FUN()>,
           warning = function(w) {
             self$log$warn <- utils::capture.output(w)[-1] |> paste(collapse = "\n")
+            if (verbose) w |> conditionMessage() |> message()
             tryInvokeRestart("muffleWarning")
           },
           params |>
